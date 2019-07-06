177677c625c3d5797eeb18cfe7642058a227d79f
KimJeongHwan <jeonghwan.kim@piolink.com>
2019-07-06 16:46:16 +0900

    2번 문제 풀이 추가

---
+++ 2_Count_of_word/count_letter.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+from __future__ import print_function 
+
+while True : 
+    # 문자열 입력   
+    print ('What is the input string ? ', end='')
+    input_string = raw_input()
+
+    # 문자열과 문자열 갯수 출력 
+    if len(input_string) == 0 :
+        # 도전과제1
+        print ('Please put in at least one letter.')
+    else :
+        print (input_string + ' has ' + str(len(input_string)) + ' characters.')
+        break
---
+++ 2_Count_of_word/count_letter_GUI.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+from __future__ import print_function 
+import tkinter as tk 
+
+window = tk.Tk()
+
+window.mainloop()
+
+
+#while True : 
+#    # 문자열 입력   
+#    print ('What is the input string ? ', end='')
+#    input_string = raw_input()
+#
+#    # 문자열과 문자열 갯수 출력 
+#    if len(input_string) == 0 :
+#        # 도전과제1
+#        print ('Please put in at least one letter.')
+#    else :
+#        print (input_string + ' has ' + str(len(input_string)) + ' characters.')
+#        break
